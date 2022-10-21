c9ce78f2a89550a367c71f27c46bc5e5c42dd7ea
codertinie <martin.munyao@moringaschool.com>
2022-10-21 18:52:24 +0300

    Completed assignment

--- lib/count_sentences.rb
+++ lib/count_sentences.rb
@@ -3,18 +3,20 @@ require 'pry'
 class String
 
   def sentence?
-    
+    self.end_with?(".") 
   end
 
   def question?
-
+    self.end_with?("?")
   end
 
   def exclamation?
-
+    self.end_with?("!")
   end
 
-  def count_sentences
-
+  def count_sentences 
+    #add a filter to only count sentences that are not empty.
+    self.split(/[.!?]/).filter {|sentence| !sentence.empty?}.length
   end
+  binding.pry
 end
